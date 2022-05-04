23dc8e4adc077347fc234a4fb9e75df65e00fce2
pramit2012 <101552755+pramit2012@users.noreply.github.com>
2022-05-04 14:18:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("My First Console Project");
+var first_name="Pramit";
+var last_name="Basu";
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var a=10;
+var b=5;
+var add=a+b;
+console.log(add);
+var sub=a-b;
+console.log(sub);
+var mul=a*b;
+console.log(mul);
+var div=a/b;
+console.log(div);
